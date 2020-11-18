7fff59f203c88df71e5270cc550c21029397c359
Guilherme Gentile <guigass@live.co.uk>
2020-11-18 20:55:23 -0300

    Ajuste nas constants

--- Paymee.API/Helpers/Constants/PaymentMethods.cs
+++ Paymee.API/Helpers/Constants/PaymentMethods.cs
@@ -6,19 +6,19 @@ namespace Paymee.API.Helpers.Constants
 {
     public static class PaymentMethods
     {
-        public static string BancoDoBrasil { get; set; } = "BB_TRANSFER";
-        public static string Bradesco { get; set; } = "BRADESCO_TRANSFER";
-        public static string Itau { get; set; } = "ITAU_TRANSFER_GENERIC";
-        public static string ItauPF { get; set; } = "ITAU_TRANSFER_PF";
-        public static string ItauPJ { get; set; } = "ITAU_TRANSFER_PJ";
-        public static string ItauDI { get; set; } = "ITAU_DI";
-        public static string Caixa { get; set; } = "CEF_TRANSFER";
-        public static string Original { get; set; } = "ORIGINAL_TRANSFER";
-        public static string Santander { get; set; } = "SANTANDER_TRANSFER";
-        public static string SantanderDI { get; set; } = "SANTANDER_DI";
-        public static string Inter { get; set; } = "INTER_TRANSFER";
-        public static string BS2 { get; set; } = "BS2_TRANSFER";
-        public static string OutrosBancos { get; set; } = "OUTROS_BANCOS";
-        public static string PIX { get; set; } = "PIX";
+        public static string BancoDoBrasil { get; } = "BB_TRANSFER";
+        public static string Bradesco { get; } = "BRADESCO_TRANSFER";
+        public static string Itau { get; } = "ITAU_TRANSFER_GENERIC";
+        public static string ItauPF { get; } = "ITAU_TRANSFER_PF";
+        public static string ItauPJ { get; } = "ITAU_TRANSFER_PJ";
+        public static string ItauDI { get; } = "ITAU_DI";
+        public static string Caixa { get; } = "CEF_TRANSFER";
+        public static string Original { get; } = "ORIGINAL_TRANSFER";
+        public static string Santander { get; } = "SANTANDER_TRANSFER";
+        public static string SantanderDI { get; } = "SANTANDER_DI";
+        public static string Inter { get; } = "INTER_TRANSFER";
+        public static string BS2 { get; } = "BS2_TRANSFER";
+        public static string OutrosBancos { get; } = "OUTROS_BANCOS";
+        public static string PIX { get; } = "PIX";
     }
 }
